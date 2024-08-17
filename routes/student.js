e16d88ebaced8d3a4413c56d0d3c4c57623370e1
RamakrishnaGithub <panchireddyp197@gmail.com>
2024-08-17 15:16:29 +0530

    dbOperationMethods

--- routes/student.js
+++ routes/student.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongodb = require("mongodb");
+const objectId = mongodb.ObjectId;
 const getDb = require('../common/dbConn');
 const router = express.Router();
 
@@ -19,12 +20,40 @@ router.post('/register', async function (req, res, next) {
 
 router.get("/get-std", async function (req, res, next) {
     try {
-        const db =await getDb();
+        const db = await getDb();
         const collection = db.collection("students")
         const result = await collection.find().toArray()
         res.send(result)
     } catch (ex) {
         res.send(ex.message)
+    }
+})
+
+router.put("/update-std", async function (req, res, next) {
+    try {
+        const id = req.query.id
+        const data = req.body.data
+        const db = await getDb()
+        const collection = db.collection("students")
+        const result = await collection.updateOne({ _id: objectId.createFromHexString(id) }, { $set: data })
+        res.send(result)
+    } catch (ex) {
+        console.error(ex)
+        res.send(ex)
+    }
+
+})
+
+router.delete("/delete-std/:id", async function (req, res, next) {
+    try {
+        const id = req.params.id
+        const db = await getDb()
+        const collection = db.collection("students")
+        const result = await collection.deleteOne({ _id: objectId.createFromHexString(id) })
+        res.send(result)
+    } catch (ex) {
+        console.error(ex)
+        res.send(ex)
     }
 })
 module.exports = router;
